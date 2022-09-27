231eeafb397ff4cce1d600444953256931b71114
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-27 22:06:32 +0300

    Add received products action

--- redux-shopping-cart-egghead/src/features/products/productsSlice.ts
+++ redux-shopping-cart-egghead/src/features/products/productsSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { Product } from "../../app/api";
+import { Products } from "./Products";
 
 export interface ProducState {
     products: { [id: string]: Product };
@@ -12,7 +13,15 @@ const initialState: ProducState = {
 const productSlice = createSlice({
     name: "products",
     initialState,
-    reducers: {}
+    reducers: {
+        recievedProducts(state, action: PayloadAction<Product[]>) {
+            const products = action.payload;
+            products.forEach(product => {
+                state.products[product.id] = product;
+            });
+        }
+    }
 });
 
+export const { recievedProducts } = productSlice.actions;
 export default productSlice.reducer;
