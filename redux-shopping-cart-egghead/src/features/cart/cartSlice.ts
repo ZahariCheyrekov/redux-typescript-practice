eba9b5a3f0c4ce57b3ecd48853a3d00cbfe08733
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-27 22:24:33 +0300

    Create get memoized num items selector

--- redux-shopping-cart-egghead/src/features/cart/cartSlice.ts
+++ redux-shopping-cart-egghead/src/features/cart/cartSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
 export interface CartState {
@@ -27,12 +27,15 @@ const cartSlice = createSlice({
 export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
 
-export function getNumItems(state: RootState) {
-    let numItems = 0;
+export const getMemoizedNumItems = createSelector(
+    (state: RootState) => state.cart.items,
+    (items) => {
+        let numItems = 0;
 
-    for (const id in state.cart.items) {
-        numItems += state.cart.items[id];
+        for (const id in items) {
+            numItems += items[id];
+        }
+
+        return numItems;
     }
-
-    return numItems;
-}
+);
