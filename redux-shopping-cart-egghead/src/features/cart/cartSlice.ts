905f0e9cc380d4e9eb0fa4ca8dd104ab556bbc12
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-28 18:29:20 +0300

    Implement remove from cart reducer and export it

--- redux-shopping-cart-egghead/src/features/cart/cartSlice.ts
+++ redux-shopping-cart-egghead/src/features/cart/cartSlice.ts
@@ -20,11 +20,14 @@ const cartSlice = createSlice({
             } else {
                 state.items[id] = 1;
             }
+        },
+        removeFromCart(state, action: PayloadAction<string>) {
+            delete state.items[action.payload];
         }
     }
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
 
 export const getMemoizedNumItems = createSelector(
@@ -49,7 +52,7 @@ export const getTotalPrice = createSelector(
         for (const id in items) {
             total += products[id].price * items[id];
         }
-        
+
         return total.toFixed(2);
     }
 );
